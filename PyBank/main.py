c01b8dbcc466243dddb92e68a0f97d2ee3897cb0
J_D_L <jocelyndeleon52@gmail.com>
2021-04-10 18:15:36 -0700

    set variables, rows in reader, and calculated total months

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,17 +8,36 @@ import csv
 # Set Path for file
 csvpath = os.path.join("Resources","budget_data.csv")
 
+#Set Variables
+month_year = []
+income_expense = []
+total_months= []
+net_income_expense = []
+average_net = []
+net_change = []
+average_net_change = []
+max_income =[]
+min_expense =[]
+
 #Open csv file and set up reader
 with open (csvpath) as csvfile:
 	csvreader = csv.reader(csvfile, delimiter= ",")
 	reader = csv.reader(csvfile)
 	next(reader, None)
 
+	for row in reader:
+		months = row[0]
+		month_year.append(months)
+		net = int(row[1])
+		income_expense.append(net)
+
 #The total number of months included in the dataset
+		total_months = len(months)
+		print(total_months)
 
 
 #The net total amount of "Profit/Losses" over the entire period
-
+		
 
 # the changes in "Profit/Losses" over the entire period, then find the average of those changes
 
@@ -34,7 +53,7 @@ with open (csvpath) as csvfile:
 
 print('Financial Analysis')
 print('-------------------------')
-print('Total Months:')
+print(f'Total Months:{total_months}')
 print('Total:')
 print('Average Change:')
 print('Greatest Increase in Profits:')
@@ -52,7 +71,7 @@ with open(output_path,'w', newline = "") as datafile:
 #Write Results
 	csvwriter.writerow(['Financial Analysis'])
 	csvwriter.writerow(['-------------------------'])
-	csvwriter.writerow('Total Months:')
+	csvwriter.writerow(f'Total Months:{total_months}')
 	csvwriter.writerow('Total:')
 	csvwriter.writerow('Average Change:')
 	csvwriter.writerow('Greatest Increase in Profits:')
